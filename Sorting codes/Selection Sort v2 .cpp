52bff5c51483b7b80551906137fc72ac3f76df03
ABU SAHID SOHAN <154542799+sohanxt@users.noreply.github.com>
2024-11-14 03:48:16 +0600

    Create Selection Sort v2 .cpp

---
+++ Sorting codes/Selection Sort v2 .cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+
+    cout << "Unsorted array:\n";
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array:\n";
+    printArray(arr, n);
+
+    return 0;
+}
