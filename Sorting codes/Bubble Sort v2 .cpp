f1ffb7da20166eac8f21f61efefa10c6f7c18200
ABU SAHID SOHAN <154542799+sohanxt@users.noreply.github.com>
2024-11-14 03:47:43 +0600

    Create Bubble Sort v2 .cpp

---
+++ Sorting codes/Bubble Sort v2 .cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+
+    cout << "Unsorted array:\n";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array:\n";
+    printArray(arr, n);
+
+    return 0;
+}
